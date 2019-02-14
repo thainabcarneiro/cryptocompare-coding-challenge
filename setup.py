0910c0b38562d80180f5dc3b8955502083b17008
Thaina Batista <thainabcareiro@gmail.com>
2019-02-14 03:39:58 -0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='cryptocompare-coding-challenge',
+    packages=['util', 'entity'],
+    author='Thainá Batista',
+    author_email='thainabcarneiro@gmail.com',
+    description='CryptoCompare.com API coding challenge'
+)
